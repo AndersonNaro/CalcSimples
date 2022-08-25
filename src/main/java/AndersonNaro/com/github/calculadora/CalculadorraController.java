930fafa848d54dc20d5e2a9ef5f49fb17733a553
80832883 <ascarranaro@minsait.com>
2022-08-25 17:28:19 -0300

    Tratamento de exceptions

--- src/main/java/AndersonNaro/com/github/calculadora/CalculadorraController.java
+++ src/main/java/AndersonNaro/com/github/calculadora/CalculadorraController.java
@@ -4,26 +4,64 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import AndersonNaro.com.github.calculadora.exceptions.UnsuportedOperationException;
+
 @RestController
 public class CalculadorraController {
 	@RequestMapping("/soma/{primeiroNumero}/{segundoNumero}")
 	public Double soma(@PathVariable String primeiroNumero, @PathVariable String segundoNumero) throws Exception {
-
-		if (!isNumeric(primeiroNumero) || !isNumeric(segundoNumero)) {
-			throw new NumberFormatException();
-		}
-
-		Double result = Double.parseDouble(primeiroNumero) + Double.parseDouble(segundoNumero);
-		return result;
+		if (!isNumeric(primeiroNumero) || !isNumeric(segundoNumero)) 
+			throw new UnsuportedOperationException("Por favor coloque dois numeros válidos!");				 		
+		return convertDouble(primeiroNumero) + convertDouble(segundoNumero);				
+	}
+	
+	@RequestMapping("/subtracao/{primeiroNumero}/{segundoNumero}")
+	public Double subtracao(@PathVariable String primeiroNumero, @PathVariable String segundoNumero) throws Exception {
+		if (!isNumeric(primeiroNumero) || !isNumeric(segundoNumero)) 
+			throw new UnsuportedOperationException("Por favor coloque dois numeros válidos!");				 		
+		return convertDouble(primeiroNumero) - convertDouble(segundoNumero);				
+	}
+	
+	@RequestMapping("/multiplicacao/{primeiroNumero}/{segundoNumero}")
+	public Double multiplicacao(@PathVariable String primeiroNumero, @PathVariable String segundoNumero) throws Exception {
+		if (!isNumeric(primeiroNumero) || !isNumeric(segundoNumero)) 
+			throw new UnsuportedOperationException("Por favor coloque dois numeros válidos!");				 		
+		return convertDouble(primeiroNumero) - convertDouble(segundoNumero);				
+	}
+	
+	@RequestMapping("/divisao/{primeiroNumero}/{segundoNumero}")
+	public Double divisao(@PathVariable String primeiroNumero, @PathVariable String segundoNumero) throws Exception {
+		if (!isNumeric(primeiroNumero) || !isNumeric(segundoNumero)) 
+			throw new UnsuportedOperationException("Por favor coloque dois numeros válidos!");				 		
+		if ( convertDouble(segundoNumero)== 0.0)
+			throw new UnsuportedOperationException("O divisor não pode ser 0!");
+		return convertDouble(primeiroNumero) / convertDouble(segundoNumero);				
+	}
+	
+	@RequestMapping("/media/{primeiroNumero}/{segundoNumero}")
+	public Double media(@PathVariable String primeiroNumero, @PathVariable String segundoNumero) throws Exception {
+		if (!isNumeric(primeiroNumero) || !isNumeric(segundoNumero)) 
+			throw new UnsuportedOperationException("Por favor coloque dois numeros válidos!");				 		
+		return (convertDouble(primeiroNumero) + convertDouble(segundoNumero))/2;				
+	}
+	
+	@RequestMapping("/raiz-quadrada/{primeiroNumero}")
+	public Double raizQuadrada(@PathVariable String primeiroNumero) throws Exception {
+		if (!isNumeric(primeiroNumero)) 
+			throw new UnsuportedOperationException("Por favor coloque um numero válido!");				 		
+		return Math.sqrt(convertDouble(primeiroNumero));				
 	}
 
-	public static boolean isNumeric(String str) {
-		try {
-			Double.parseDouble(str);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
+	private static Double convertDouble(String strNum) {
+		if (strNum == null) return 0.0;
+		String num = strNum.replace(",",".");
+		return Double.parseDouble(num);
+	}
+
+	private static boolean isNumeric(String strNum) {
+		if (strNum == null) return false;
+		String num = strNum.replace(",",".");		
+		return num.matches("[-+]?[0-9]*\\.?[0-9]+");	//regex que pega números
 	}
 
 }
---
+++ src/main/java/AndersonNaro/com/github/calculadora/exceptions/UnsuportedOperationException.java
@@ -0,0 +1,15 @@
+package AndersonNaro.com.github.calculadora.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UnsuportedOperationException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	public UnsuportedOperationException(String exception) {
+		super(exception);
+	}
+
+}
